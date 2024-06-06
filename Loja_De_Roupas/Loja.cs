68d36dc1d03296a7fba16be66e686e7559a19692
Juliana <109084569+AngeloHervis@users.noreply.github.com>
2024-06-06 20:25:05 -0300

    Classes e objetos criados

--- Loja_De_Roupas/Loja.cs
+++ Loja_De_Roupas/Loja.cs
@@ -2,5 +2,16 @@
 
 public class Loja
 {
-    
+    public int Id { get; }
+    public string Nome { get; set; }
+    public int TotalClientes { get; set; }
+    public List<Cliente> Clientes { get; set; }
+    public List<Roupa> Roupas { get; set; }
+
+    public Loja()
+    {
+        Clientes = new List<Cliente>();
+        Roupas = new List<Roupa>();
+    }
+
 }
